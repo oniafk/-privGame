b0edf03ea50f73b7345f70fe1852b5baa7f38796
Onimae <manufelipearias@gmail.com>
2023-01-21 22:39:45 -0500

    changing startGame function

--- game.js
+++ game.js
@@ -28,15 +28,27 @@ function startGame() {
     game.font = elementSize + 'px verdana';
     game.textAlign = 'end';
 
-    let map = maps [1];
+    let map = maps [2];
     let mapRows = map.trim().split('\n');
     let mapRowToCol = mapRows.map(row => row.trim().split(''));
 
-    for (let  row = 1;  row <= 10 ; row ++) {       
-        for (let col = 1; col <= 10; col++) {
-            game.fillText(emojis[mapRowToCol [row -1] [col -1]], elementSize * col, elementSize * row)
-        }        
-    }
+    mapRowToCol.forEach( (row, rowIndex) => {
+        row.forEach( (col, colIndex) => {
+
+            let posX = elementSize * (colIndex + 1);
+            let posY = elementSize * (rowIndex + 1);        
+            let emoji = emojis [col];
+
+            game.fillText(emoji, posX, posY);
+
+        });
+    })
+
+    // for (let  row = 1;  row <= 10 ; row ++) {       
+    //     for (let col = 1; col <= 10; col++) {
+    //         game.fillText(emojis[mapRowToCol [row -1] [col -1]], elementSize * col, elementSize * row)
+    //     }        
+    // }
     
 };
 
