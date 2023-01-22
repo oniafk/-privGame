1a67f8c0b3d0029ab13439bd870f05abf5ff79f7
Onimae <manufelipearias@gmail.com>
2023-01-21 23:53:28 -0500

    cecking movement

--- game.js
+++ game.js
@@ -1,11 +1,23 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
 
 let canvasSize;
 let elementSize;
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
+
+document.onkeydown = checkKey;
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+// map and canvas
 
 function setCanvasSize() {
 
@@ -42,13 +54,31 @@ function startGame() {
             game.fillText(emoji, posX, posY);
 
         });
-    })
-
-    // for (let  row = 1;  row <= 10 ; row ++) {       
-    //     for (let col = 1; col <= 10; col++) {
-    //         game.fillText(emojis[mapRowToCol [row -1] [col -1]], elementSize * col, elementSize * row)
-    //     }        
-    // }
-    
+    });      
 };
 
+//moving
+
+function checkKey (event) {
+    event = event || window.event;
+    if(event.keyCode == '38') moveUp();
+    else if(event.keyCode == '40') moveDown();
+    else if(event.keyCode == '37') moveLeft();
+    else if(event.keyCode == '39') moveRight();
+}
+
+function moveUp() {
+    console.log('up')
+};
+
+function moveLeft() {
+    console.log('left')
+};
+
+function moveRight() {
+    console.log('right')
+};
+
+function moveDown() {
+    console.log('down')
+};
